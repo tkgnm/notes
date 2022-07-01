3af5ab7edcccfdc9bfed736b389a3b874fb6894b
tkgnm <thomas.king@guardian.co.uk>
2022-07-01 20:41:47 +0100

    Initial Commit

---
+++ experiment wiht offset/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  experiment wiht offset
+//
+//  Created by Thomas King on 01/07/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ experiment wiht offset/experiment_wiht_offsetApp.swift
@@ -0,0 +1,17 @@
+//
+//  experiment_wiht_offsetApp.swift
+//  experiment wiht offset
+//
+//  Created by Thomas King on 01/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct experiment_wiht_offsetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
