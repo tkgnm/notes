aba00f458126fe972f14c42b3749839617c26227
Thomas King <102960825+tkgnm@users.noreply.github.com>
2022-08-29 19:32:34 +0100

    created answer struct

---
+++ notes/Models/Answer.swift
@@ -0,0 +1,13 @@
+//
+//  Answer.swift
+//  experiment wiht offset
+//
+//  Created by Thomas King on 29/08/2022.
+//
+
+import Foundation
+
+struct Answer {
+    var answer: String
+    var isCorrect: Bool
+}
